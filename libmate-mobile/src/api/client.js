3eca27bc7a305465bd354a57f50e3832749ecf62
Prince Vaishya <princebaishya1234@gmail.com>
2026-04-19 20:51:09 +0545

    modified .env and client.js file

--- libmate-mobile/src/api/client.js
+++ libmate-mobile/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-const API_BASE_URL = process.env.API_BASE_URL || 'http://192.168.1.64:5000/api';
+const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || 'http://192.168.1.85:5000/api';
 
 const client = axios.create({
   baseURL: API_BASE_URL,
