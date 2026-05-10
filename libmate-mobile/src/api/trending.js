2711c0d226af99026c7d3750c8ecc264c3ee6547
Prince Vaishya <princebaishya1234@gmail.com>
2026-05-10 10:15:25 +0545

    update libmate file directory

--- libmate-mobile/src/api/trending.js
+++
@@ -1,9 +0,0 @@
-import client from './client';
-
-export const getTrending = (limit = 10) =>
-  client.get('/trending', { params: { limit } });
-
-export const getNewArrivals = (limit = 6) =>
-  client.get('/new-arrivals/latest', { params: { limit } });
-
-export const getStats = () => client.get('/trending/stats');
